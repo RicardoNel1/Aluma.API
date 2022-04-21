cafb8e5db81c8fa029e6da2b32db3a28cf3de0d8
Johan Koster <johan@fintegratetech.co.za>
2022-04-21 16:14:07 +0200

    show swagger uat

--- Aluma.API/Startup.cs
+++ Aluma.API/Startup.cs
@@ -74,9 +74,7 @@ namespace Aluma.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                //dev only move back
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Aluma.API v1.0.0"));
+                
 
                 app.UseHangfireDashboard();
 
@@ -85,6 +83,10 @@ namespace Aluma.API
             {
                 app.UseHsts();
             }
+            //dev only move back
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Aluma.API UATv1.0.0"));
+
             //app.UseHttpsRedirection();
 
             app.UseStaticFiles();
