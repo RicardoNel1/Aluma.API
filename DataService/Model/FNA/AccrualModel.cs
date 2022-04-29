41bf96f9eb6832d8187331bc3efef48281f974d2
Vivian Hodgkinson <vivian@fintegratetech.co.za>
2022-04-29 12:39:28 +0200

    Added missing inheritance

--- DataService/Model/FNA/AccrualModel.cs
+++ DataService/Model/FNA/AccrualModel.cs
@@ -6,7 +6,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace DataService.Model
 {
     [Table("accrual")]
-    public class AccrualModel
+    public class AccrualModel : BaseModel
     {
         public int Id { get; set; }
         public int ClientId { get; set; }
@@ -29,6 +29,10 @@ namespace DataService.Model
         {
             builder.HasKey(x => x.Id);
             builder.Property(x => x.Id).ValueGeneratedOnAdd();
+
+            builder.HasIndex(c => c.ClientId).IsUnique();
+
+
         }
     }
 }
