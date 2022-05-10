98f0e1c77487e95ab39aedd3cdf94b322a459ccc
Justin Scheepers <justin@fintegratetech.co.za>
2022-05-10 11:07:54 +0200

    #629

--- DataService/Dto/FNA/RetirementPreservationFundsDto.cs
+++ DataService/Dto/FNA/RetirementPreservationFundsDto.cs
@@ -9,6 +9,7 @@ namespace DataService.Dto
         public int ClientId { get; set; }
         public string Description { get; set; }
         public double Value { get; set; }
+        public double Growth { get; set; }
 
     }
 
--- DataService/Model/FNA/RetirementPreservationFundsModel.cs
+++ DataService/Model/FNA/RetirementPreservationFundsModel.cs
@@ -15,6 +15,7 @@ namespace DataService.Model
         public int ClientId { get; set; }
         public string Description { get; set; }
         public double Value { get; set; }
+        public double Growth { get; set; }
 
     }
 
