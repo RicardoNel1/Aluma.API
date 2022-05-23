f58ec6ac801d2f6a228b1c20e163082b6bed97db
Arno du Toit <arno@fintegratetech.co.za>
2022-05-23 12:11:04 +0200

    AB#570

--- Aluma.API/Repositories/FNA/RetirementPlanningRepo.cs
+++ Aluma.API/Repositories/FNA/RetirementPlanningRepo.cs
@@ -58,14 +58,15 @@ namespace Aluma.API.Repositories
 
         public RetirementPlanningDto GetRetirementPlanning(int fnaId)
         {
-            try {
-            RetirementPlanningModel data = _context.RetirementPlanning.Where(c => c.FNAId == fnaId).First();
-            return _mapper.Map<RetirementPlanningDto>(data);
+            try
+            {
+                RetirementPlanningModel data = _context.RetirementPlanning.Where(c => c.FNAId == fnaId).First();
+                return _mapper.Map<RetirementPlanningDto>(data);
             }
-            catch(Exception ex) {
+            catch (Exception ex)
+            {
                 return null;
             }
-
         }
 
         public RetirementPlanningDto UpdateRetirementPlanning(RetirementPlanningDto dto)
