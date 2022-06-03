56765b39cf59fcd7e00ac2d723bdf7fc416ed6ef
Arno du Toit <arno@fintegratetech.co.za>
2022-06-03 09:20:18 +0200

    AB#733 added Total Capital available on retirement planning

--- DataService/Dto/FNA/RetirementPlanningDto.cs
+++ DataService/Dto/FNA/RetirementPlanningDto.cs
@@ -4,7 +4,7 @@ using DataService.Enum;
 namespace DataService.Dto
 {
 
-    public class RetirementPlanningDto: ApiResponseDto
+    public class RetirementPlanningDto : ApiResponseDto
     {
         public int Id { get; set; }
         public int FNAId { get; set; }
@@ -14,7 +14,8 @@ namespace DataService.Dto
         public double IncomeNeeds { get; set; }
         public int NeedsTerm_Years { get; set; }
         public double IncomeNeedsEscalation { get; set; }
-        public double CapitalNeeds { get; set; }
+        public double TotalCapitalNeeds { get; set; }
+        public double TotalCapitalAvailable { get; set; }
         public double OutstandingLiabilities { get; set; }
         public double SavingsEscalation { get; set; }
     }
