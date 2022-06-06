3ef50c71100ceaa97be2dd389301e76fe2c00af3
Arno du Toit <arno@fintegratetech.co.za>
2022-06-06 12:19:09 +0200

    AB#607 Populate Personal Detail HTML

--- DataService/Dto/FNA/Report/PersonalDetailReportDto.cs
+++ DataService/Dto/FNA/Report/PersonalDetailReportDto.cs
@@ -9,26 +9,17 @@ namespace DataService.Dto
     public class PersonalDetailReportDto
     {
         public string FirstName { get; set; }
-        public string Lastname { get; set; }
-        public string SpouseFirstName { get; set; }
-        public string SpouseLastName { get; set; }
+        public string LastName { get; set; }
         public string RSAIdNumber { get; set; }
-        public string SpouseRSAIdNumber { get; set; }
         public string DateOfBirth { get; set; }
-        public string ClientAge { get; set; }
-        public string SpouseClientAge { get; set; }
+        public string Age { get; set; }
         public string Gender { get; set; }
-        public string SpouseGender { get; set; }
         public string LifeExpectancy { get; set; }
         public string MaritalStatus { get; set; }
-        public string SpouseMaritalStatus { get; set; }
         public string DateOfMarriage { get; set; }
-        public string SpouseDateOfMarriage { get; set; }
         public string Email { get; set; }
-        public string SpouseEmail { get; set; }
         public string WorkNumber { get; set; }
-        public string SpouseWorkNumber { get; set; }
-        public string ClientAddress { get; set; }
-        public string ClientPostal { get; set; }
+        public string Address { get; set; }
+        public string Postal { get; set; }
     }
 }
