87e7d729076a3266af377788a042d3f7e6a16183
Arno du Toit <arno@fintegratetech.co.za>
2022-06-06 15:38:38 +0200

    AB#607 Added Graph Data

--- DataService/Dto/FNA/Report/GraphReportDto.cs
+++ DataService/Dto/FNA/Report/GraphReportDto.cs
@@ -10,6 +10,14 @@ namespace DataService.Dto
     {
         public string Type { get; set; }
         public string Name { get; set; }
+        public string XaxisHeader { get; set; }
+        public string YaxisHeader { get; set; }
         public List<Dictionary<string, string>> Data { get; set; }
+    }
+
+    public class GraphResult
+    {
+        public string Html { get; set; }
+        public string Script { get; set; }
     }
 }
