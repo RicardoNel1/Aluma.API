c248760eb34985c768bfe28d1a4071fdc9506dbd
Justin Scheepers <justin@fintegratetech.co.za>
2022-06-24 09:04:24 +0200

    #863

--- DataService/Dto/Client/ClientPortfolioDto.cs
+++ DataService/Dto/Client/ClientPortfolioDto.cs
@@ -16,22 +16,22 @@ namespace DataService.Dto
         public MedicalAidDTO MedicalAid { get; set; }
 
         public double Salary { get; set; }
-        public double PensionFunds { get; set; }
-        public double PreservationFunds { get; set; }
+        //public double PensionFunds { get; set; }
+        //public double PreservationFunds { get; set; }
         public double FuneralCover { get; set; }
-        public double DisabilityLumpSum { get; set; }
-        public double DisabilityTemporary { get; set; }
-        public double DisabilityPermanent { get; set; }
+        //public double DisabilityLumpSum { get; set; }
+        //public double DisabilityTemporary { get; set; }
+        //public double DisabilityPermanent { get; set; }
         public double HomeValue { get; set; }
-        public double InvestmentsTotal { get; set; }
-        public double InvestedBonds { get; set; }
-        public double InvestedEuity { get; set; }
-        public double InvestedProperty { get; set; }
-        public double InvestedOffshoreBonds { get; set; }
-        public double InvestedOffshoreEuity { get; set; }
-        public double InvestedOffshoreProperty { get; set; }
-        public double InvestedPrivateEquity { get; set; }
-        public double InvestedCash { get; set; }
+        //public double InvestmentsTotal { get; set; }
+        //public double InvestedBonds { get; set; }
+        //public double InvestedEuity { get; set; }
+        //public double InvestedProperty { get; set; }
+        //public double InvestedOffshoreBonds { get; set; }
+        //public double InvestedOffshoreEuity { get; set; }
+        //public double InvestedOffshoreProperty { get; set; }
+        //public double InvestedPrivateEquity { get; set; }
+        //public double InvestedCash { get; set; }
 
 
 
