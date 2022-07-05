1ee3b25d5606d96ac56f1f0fd447adcd61d6b71d
Arno du Toit <arno@fintegratetech.co.za>
2022-07-05 11:27:44 +0200

    added liquid assets to portfolio

--- DataService/Dto/Client/ClientPortfolioDto.cs
+++ DataService/Dto/Client/ClientPortfolioDto.cs
@@ -9,7 +9,7 @@ namespace DataService.Dto
         public ClientFNADto FNA { get; set; }
         public List<InvestmentsDto> Investments { get; set; }
         public RetirementSummaryDto Retirement { get; set; }
-        public RetirementPlanningDto RetirementPlanning { get; set;}
+        public RetirementPlanningDto RetirementPlanning { get; set; }
         public ProvidingOnDisabilityDto ProvidingDisability { get; set; }
         public ProvidingOnDeathDto ProvidingDeath { get; set; }
         public ProvidingOnDreadDiseaseDto ProvidingDread { get; set; }
@@ -17,6 +17,7 @@ namespace DataService.Dto
         public MedicalAidDTO MedicalAid { get; set; }
         public List<AssetsAttractingCGTDto> AssetsAttractingCGT { get; set; }
         public List<AssetsExemptFromCGTDto> AssetsExemptFromCGT { get; set; }
+        public List<LiquidAssetsDto> LiquidAssets{ get; set; }
         public List<InsuranceDto> Insurance { get; set; }
         public AssumptionsDto Assumptions { get; set; }
         public PrimaryResidenceDto PrimaryResidence { get; set; }
