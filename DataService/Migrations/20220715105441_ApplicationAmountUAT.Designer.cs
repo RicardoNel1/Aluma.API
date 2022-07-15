d84d70f8ca01b8961ef38a20a34d8ebf38fdc1ee
Justin Scheepers <justin@fintegratetech.co.za>
2022-07-15 12:59:51 +0200

    #892 done

--- DataService/Migrations/20220715105441_ApplicationAmountUAT.Designer.cs
+++ DataService/Migrations/20220715105441_ApplicationAmountUAT.Designer.cs
@@ -4,14 +4,16 @@ using DataService.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataService.Migrations
 {
     [DbContext(typeof(AlumaDBContext))]
-    partial class AlumaDBContextModelSnapshot : ModelSnapshot
+    [Migration("20220715105441_ApplicationAmountUAT")]
+    partial class ApplicationAmountUAT
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
