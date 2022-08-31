def35ced700ab000dca8d364b4755ccec5ef8df3
Justin Scheepers <justin@fintegratetech.co.za>
2022-08-31 08:53:11 +0200

    Added PE1 & PE2 quotes + formulas

--- Aluma.API/Repositories/Product/PEFRepo.cs
+++ Aluma.API/Repositories/Product/PEFRepo.cs
@@ -134,48 +134,97 @@ namespace Aluma.API.Repositories
             {
                 //Calculations
                 double i = product.AcceptedLumpSum;
-                double r1 = .129;
-                double r2 = .1895;
+                double r1 = .132;
+                double r2 = .191;
+                double dt = .2;
+
                 double dividendYear1Gross1 = i * r1;
+                double dividendYear1Nett1 = dividendYear1Gross1 - (dividendYear1Gross1*dt);
+
                 double dividendYear2Gross1 = (i + dividendYear1Gross1) * r1;
+                double dividendYear2Nett1 = dividendYear2Gross1 - (dividendYear2Gross1 * dt);
+
                 double dividendYear3Gross1 = (i + dividendYear1Gross1 + dividendYear2Gross1) * r1;
+                double dividendYear3Nett1 = dividendYear3Gross1 - (dividendYear3Gross1 * dt);
+
                 double dividendYear4Gross1 = (i + dividendYear1Gross1 + dividendYear2Gross1 + dividendYear3Gross1) * r1;
+                double dividendYear4Nett1 = dividendYear4Gross1 - (dividendYear4Gross1 * dt);
+
                 double dividendYear5Gross1 = (i + dividendYear1Gross1 + dividendYear2Gross1 + dividendYear3Gross1 + dividendYear4Gross1) * r1;
+                double dividendYear5Nett1 = dividendYear5Gross1 - (dividendYear5Gross1 * dt);
+
                 double totalCumulativeDividendGross1 = dividendYear1Gross1 + dividendYear2Gross1 + dividendYear3Gross1 + dividendYear4Gross1 + dividendYear5Gross1;
+                double totalCumulativeDividendNett1 = dividendYear1Nett1 + dividendYear2Nett1 + dividendYear3Nett1 + dividendYear4Nett1 + dividendYear5Nett1;
+
                 double totalPayoutMaturityGross1 = totalCumulativeDividendGross1 + i;
+                double totalPayoutMaturityNett1 = totalCumulativeDividendNett1 + i;
 
                 double dividendYear1Gross2 = i * r2;
+                double dividendYear1Nett2 = dividendYear1Gross2 - (dividendYear1Gross2 * dt);
+
                 double dividendYear2Gross2 = (i + dividendYear1Gross2) * r2;
+                double dividendYear2Nett2 = dividendYear2Gross2 - (dividendYear2Gross2 * dt);
+
                 double dividendYear3Gross2 = (i + dividendYear1Gross2 + dividendYear2Gross2) * r2;
+                double dividendYear3Nett2 = dividendYear3Gross2 - (dividendYear3Gross2 * dt);
+
                 double dividendYear4Gross2 = (i + dividendYear1Gross2 + dividendYear2Gross2 + dividendYear3Gross2) * r2;
+                double dividendYear4Nett2 = dividendYear4Gross2 - (dividendYear4Gross2 * dt);
+
                 double dividendYear5Gross2 = (i + dividendYear1Gross2 + dividendYear2Gross2 + dividendYear3Gross2 + dividendYear4Gross2) * r2;
+                double dividendYear5Nett2 = dividendYear5Gross2 - (dividendYear5Gross2 * dt);
+
                 double totalCumulativeDividendGross2 = dividendYear1Gross2 + dividendYear2Gross2 + dividendYear3Gross2 + dividendYear4Gross2 + dividendYear5Gross2;
+                double totalCumulativeDividendNett2 = dividendYear1Nett2 + dividendYear2Nett2 + dividendYear3Nett2 + dividendYear4Nett2 + dividendYear5Nett2;
+
                 double totalPayoutMaturityGross2 = totalCumulativeDividendGross2 + i;
+                double totalPayoutMaturityNett2 = totalCumulativeDividendNett2 + i;
 
 
                 d[$"initialInvestment"] = "R " + i.ToString("F");
-                d[$"1dividendYear1Gross"] = "R " + dividendYear1Gross1.ToString("F");
-                d[$"1dividendYear2Gross"] = "R " + dividendYear2Gross1.ToString("F");
-                d[$"1dividendYear3Gross"] = "R " + dividendYear3Gross1.ToString("F");
-                d[$"1dividendYear4Gross"] = "R " + dividendYear4Gross1.ToString("F");
-                d[$"1dividendYear5Gross"] = "R " + dividendYear5Gross1.ToString("F");
-                d[$"1totalCumulativeDividendGross"] = "R " + totalCumulativeDividendGross1.ToString("F");
-                d[$"1totalPayoutMaturityGross"] = "R " + totalPayoutMaturityGross1.ToString("F");
+                d[$"1dividendYear1Gross"] = "R " + dividendYear1Gross1.ToString("N");
+                d[$"1dividendYear2Gross"] = "R " + dividendYear2Gross1.ToString("N");
+                d[$"1dividendYear3Gross"] = "R " + dividendYear3Gross1.ToString("N");
+                d[$"1dividendYear4Gross"] = "R " + dividendYear4Gross1.ToString("N");
+                d[$"1dividendYear5Gross"] = "R " + dividendYear5Gross1.ToString("N");
+                d[$"1totalCumulativeDividendGross"] = "R " + totalCumulativeDividendGross1.ToString("N");
+                d[$"1totalPayoutMaturityGross"] = "R " + totalPayoutMaturityGross1.ToString("N");
 
-                d[$"2dividendYear1Gross"] = "R " + dividendYear1Gross2.ToString("F");
-                d[$"2dividendYear2Gross"] = "R " + dividendYear2Gross2.ToString("F");
-                d[$"2dividendYear3Gross"] = "R " + dividendYear3Gross2.ToString("F");
-                d[$"2dividendYear4Gross"] = "R " + dividendYear4Gross2.ToString("F");
-                d[$"2dividendYear5Gross"] = "R " + dividendYear5Gross2.ToString("F");
-                d[$"2totalCumulativeDividendGross"] = "R " + totalCumulativeDividendGross2.ToString("F");
-                d[$"2totalPayoutMaturityGross"] = "R " + totalPayoutMaturityGross2.ToString("F");
+                d[$"2dividendYear1Gross"] = "R " + dividendYear1Gross2.ToString("N");
+                d[$"2dividendYear2Gross"] = "R " + dividendYear2Gross2.ToString("N");
+                d[$"2dividendYear3Gross"] = "R " + dividendYear3Gross2.ToString("N");
+                d[$"2dividendYear4Gross"] = "R " + dividendYear4Gross2.ToString("N");
+                d[$"2dividendYear5Gross"] = "R " + dividendYear5Gross2.ToString("N");
+                d[$"2totalCumulativeDividendGross"] = "R " + totalCumulativeDividendGross2.ToString("N");
+                d[$"2totalPayoutMaturityGross"] = "R " + totalPayoutMaturityGross2.ToString("N");
+
+
+
+                d[$"1dividendYear1Nett"] = "R " + dividendYear1Nett1.ToString("N");
+                d[$"1dividendYear2Nett"] = "R " + dividendYear2Nett1.ToString("N");
+                d[$"1dividendYear3Nett"] = "R " + dividendYear3Nett1.ToString("N");
+                d[$"1dividendYear4Nett"] = "R " + dividendYear4Nett1.ToString("N");
+                d[$"1dividendYear5Nett"] = "R " + dividendYear5Nett1.ToString("N");
+                d[$"1totalCumulativeDividendNett"] = "R " + totalCumulativeDividendNett1.ToString("N");
+                d[$"1totalPayoutMaturityNett"] = "R " + totalPayoutMaturityNett1.ToString("N");
+
+                d[$"2dividendYear1Nett"] = "R " + dividendYear1Nett2.ToString("N");
+                d[$"2dividendYear2Nett"] = "R " + dividendYear2Nett2.ToString("N");
+                d[$"2dividendYear3Nett"] = "R " + dividendYear3Nett2.ToString("N");
+                d[$"2dividendYear4Nett"] = "R " + dividendYear4Nett2.ToString("N");
+                d[$"2dividendYear5Nett"] = "R " + dividendYear5Nett2.ToString("N");
+                d[$"2totalCumulativeDividendNett"] = "R " + totalCumulativeDividendNett2.ToString("N");
+                d[$"2totalPayoutMaturityNett"] = "R " + totalPayoutMaturityNett2.ToString("N");
+
+
+
             }
 
             if (product.ProductId == 6)
             {
                 //Calculations                
                 double i = product.AcceptedLumpSum;
-                double r = .129;
+                double r = .132;
                 double ab = .025;
                 double dt = .2;
                 double t = 5;
@@ -190,16 +239,16 @@ namespace Aluma.API.Repositories
                 double dividendPayoutNett = dividendPayoutGross - (dividendPayoutGross * dt);
 
 
-                d[$"initialInvestment"] = "R " + i.ToString("F");
+                d[$"initialInvestment"] = "R " + i.ToString("N");
 
-                d[$"monthlyDividendGross"] = "R " + monthlyDividendGross.ToString("F");
-                d[$"monthlyDividendNett"] = "R " + monthlyDividendNett.ToString("F");
-                d[$"monthlyDividendAnnualGross"] = "R " + monthlyDividendAnnualGross.ToString("F");
-                d[$"monthlyDividendAnnualNett"] = "R " + monthlyDividendAnnualNett.ToString("F");
-                d[$"annualBonusGross"] = "R " + annualBonusGross.ToString("F");
-                d[$"annualBonusNet"] = "R " + annualBonusNet.ToString("F");
-                d[$"dividendPayoutGross"] = "R " + dividendPayoutGross.ToString("F");
-                d[$"dividendPayoutNett"] = "R " + dividendPayoutNett.ToString("F");
+                d[$"monthlyDividendGross"] = "R " + monthlyDividendGross.ToString("N");
+                d[$"monthlyDividendNett"] = "R " + monthlyDividendNett.ToString("N");
+                d[$"monthlyDividendAnnualGross"] = "R " + monthlyDividendAnnualGross.ToString("N");
+                d[$"monthlyDividendAnnualNett"] = "R " + monthlyDividendAnnualNett.ToString("N");
+                d[$"annualBonusGross"] = "R " + annualBonusGross.ToString("N");
+                d[$"annualBonusNet"] = "R " + annualBonusNet.ToString("N");
+                d[$"dividendPayoutGross"] = "R " + dividendPayoutGross.ToString("N");
+                d[$"dividendPayoutNett"] = "R " + dividendPayoutNett.ToString("N");
 
             }
             //quoteNumber
